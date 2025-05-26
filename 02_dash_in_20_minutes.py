17b2e3c590d7a5a82fee33dd2a291ea0c5914777
Syed Qasim Ali Shah <syedqasimali311@gmail.com>
2025-05-26 12:12:12 +0200

    controls and callbacks

--- 02_dash_in_20_minutes.py
+++ 02_dash_in_20_minutes.py
@@ -1,5 +1,5 @@
 # Import packages
-from dash import Dash, html, dash_table, dcc
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
 
@@ -11,12 +11,22 @@ app = Dash()
 
 # App layout
 app.layout = [
-    html.Div(children='My First App with Data and a Graph'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10),
-    dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg'))
+    html.Div(children='My First App with Data, Graph, and Controls'),
+    html.Hr(),
+    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=6),
+    dcc.Graph(figure={}, id='controls-and-graph')
 ]
+
+# Add controls to build the interaction
+@callback(
+    Output(component_id='controls-and-graph', component_property='figure'),
+    Input(component_id='controls-and-radio-item', component_property='value')
+)
+def update_graph(col_chosen):
+    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+    return fig
 
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)
-    
